b78f06d0e70b81fb1d1618f455b69fda830a01fe
JasonChien <s124202468@gmail.com>
2018-02-25 12:10:05 +0800

    Add activity.

---
+++ app/src/main/java/com/chiachen/portfolio/ProgressButtonActivity.java
@@ -0,0 +1,13 @@
+package com.chiachen.portfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_button);
+    }
+}
