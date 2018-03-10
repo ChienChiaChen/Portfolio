690d56b0712b43b3ae70b1e8e7cbc27eac73b616
JasonChien <s124202468@gmail.com>
2018-03-10 16:03:31 +0800

    8-2. Add constant class.

--- app/src/main/java/com/chiachen/portfolio/activity/EditTextActivity.java
+++ app/src/main/java/com/chiachen/portfolio/activity/EditTextActivity.java
@@ -9,13 +9,11 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 
 import com.chiachen.portfolio.R;
+import com.chiachen.portfolio.utils.Constant;
 import com.chiachen.portfolio.utils.tool.DensityUtil;
 
 // Refer:https://medium.com/joe-tsai/%E7%94%A8-constraintlayout-%E8%A7%A3%E6%B1%BA%E9%8D%B5%E7%9B%A4%E6%93%8B%E4%BD%8F%E8%BC%B8%E5%85%A5%E6%A1%86%E5%8F%8A%E6%8C%89%E9%88%95-642fe2a9b855
 public class EditTextActivity extends AppCompatActivity {
-    public static final int STARTUP_DELAY = 300;
-    public static final int ANIM_ITEM_DURATION = 1000;
-    public static final int ITEM_DELAY = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +27,8 @@ public class EditTextActivity extends AppCompatActivity {
     private void startHeaderAnim() {
         ViewCompat.animate(findViewById(R.id.layout_header))
                 .translationY(-(DensityUtil.dp2px(EditTextActivity.this,80)))// 80dp
-                .setStartDelay(STARTUP_DELAY)
-                .setDuration(ANIM_ITEM_DURATION)
+                .setStartDelay(Constant.STARTUP_DELAY)
+                .setDuration(Constant.ANIM_ITEM_DURATION)
                 .setInterpolator(new DecelerateInterpolator(1.2f))
                 .start();
     }
@@ -42,16 +40,16 @@ public class EditTextActivity extends AppCompatActivity {
                 ViewCompat.animate(view)
                         .translationY(50)
                         .alpha(1)
-                        .setStartDelay((ITEM_DELAY * i) + 500)
-                        .setDuration(ANIM_ITEM_DURATION)
+                        .setStartDelay((Constant.ITEM_DELAY * i) + 500)
+                        .setDuration(Constant.ANIM_ITEM_DURATION)
                         .setInterpolator(new DecelerateInterpolator())
                         .start();
             } else {
                 ViewCompat.animate(view)
                         .scaleY(1)
                         .scaleX(1)
-                        .setStartDelay((ITEM_DELAY * i) + 500)
-                        .setDuration(ANIM_ITEM_DURATION / 2)
+                        .setStartDelay((Constant.ITEM_DELAY * i) + 500)
+                        .setDuration(Constant.ANIM_ITEM_DURATION / 2)
                         .setInterpolator(new DecelerateInterpolator())
                         .start();
             }
---
+++ app/src/main/java/com/chiachen/portfolio/utils/Constant.java
@@ -0,0 +1,8 @@
+package com.chiachen.portfolio.utils;
+
+
+public interface Constant {
+    int STARTUP_DELAY = 300;
+    int ANIM_ITEM_DURATION = 1000;
+    int ITEM_DELAY = 300;
+}
