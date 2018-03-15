ace41b09c65f05e98eec6b9f9a8c7f1522853113
JasonChien <s124202468@gmail.com>
2018-03-15 15:32:41 +0800

    10-1. create.

--- app/src/main/java/com/chiachen/portfolio/activity/BaseActivity.java
+++ app/src/main/java/com/chiachen/portfolio/activity/BaseActivity.java
@@ -3,6 +3,8 @@ package com.chiachen.portfolio.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -21,5 +23,13 @@ public class BaseActivity extends AppCompatActivity {
 
     public void hideLoading() {
 
+    }
+
+    protected void logJJ(String msg) {
+        if (TextUtils.isEmpty(msg)) {
+            return;
+        }
+
+        Log.e("JASON_CHIEN", "\n"+msg);
     }
 }
--- app/src/main/java/com/chiachen/portfolio/activity/rxjava/RxJavaCreateActivity.java
+++ app/src/main/java/com/chiachen/portfolio/activity/rxjava/RxJavaCreateActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 
 import com.chiachen.portfolio.R;
 import com.chiachen.portfolio.activity.BaseActivity;
+import com.chiachen.portfolio.network.AppSchedulerProvider;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
 
 public class RxJavaCreateActivity extends BaseActivity {
 
@@ -11,5 +17,23 @@ public class RxJavaCreateActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_java_create);
+
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                    for (int i = 0; i < 5; i++) {
+                        logJJ(Thread.currentThread().getName());
+                        e.onNext(String.valueOf(i));
+                    }
+                }
+            })
+            .subscribeOn(AppSchedulerProvider.io())
+            .observeOn(AppSchedulerProvider.ui())
+            .subscribe(new Consumer<String>() {
+                @Override
+                public void accept(String s) throws Exception {
+                    logJJ(s + "  " + Thread.currentThread().getName());
+                }
+            });
     }
 }
