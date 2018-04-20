086edd6379553d4da140258b2acf5f6d035caf11
JasonChien <s124202468@gmail.com>
2018-04-21 05:20:50 +0800

    21. dagger2 example -5 Qualifier.

---
+++ app/src/main/java/com/chiachen/portfolio/activity/di/annotation/RedCloth.java
@@ -0,0 +1,15 @@
+package com.chiachen.portfolio.activity.di.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by jianjiacheng on 21/04/2018.
+ */
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RedCloth {
+}
