2c9a77ba9ea53993a9ec87bf12ec5ec18b2f942f
JasonChien <s124202468@gmail.com>
2018-07-06 15:52:34 +0800

    Add disposable.

--- app/src/main/java/com/chiachen/portfolio/activity/rxjava/RxJavaIntervalActivity.java
+++ app/src/main/java/com/chiachen/portfolio/activity/rxjava/RxJavaIntervalActivity.java
@@ -12,14 +12,15 @@ import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 // https://www.jianshu.com/p/b39afa92807e
 public class RxJavaIntervalActivity extends BaseActivity {
 
-    TextView mRxOperatorsText;
-
+    private TextView mRxOperatorsText;
+    private Disposable disposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +28,7 @@ public class RxJavaIntervalActivity extends BaseActivity {
         mRxOperatorsText = findViewById(R.id.text);
         mRxOperatorsText.append("timer start : " + TimeUtil.getNowStrTime() + "\n");
         logJJ( "timer start : " + TimeUtil.getNowStrTime() + "\n");
-        Observable.interval(3,2, TimeUnit.SECONDS)
+        disposable = Observable.interval(3,2, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread()) // 由于interval默认在新线程，所以我们应该切回主线程
                 .subscribe(new Consumer<Long>() {
@@ -38,5 +39,11 @@ public class RxJavaIntervalActivity extends BaseActivity {
                     }
                 });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
