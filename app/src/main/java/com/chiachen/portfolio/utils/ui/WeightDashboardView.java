25f683067d29c78b1a60adf26721be7637e1c330
JasonChien <s124202468@gmail.com>
2018-07-07 09:19:44 +0800

    Add custom view 2.

--- app/src/main/java/com/chiachen/portfolio/utils/ui/WeightDashboardView.java
+++ app/src/main/java/com/chiachen/portfolio/utils/ui/WeightDashboardView.java
@@ -2,48 +2,41 @@ package com.chiachen.portfolio.utils.ui;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.view.View;
 
-import com.chiachen.portfolio.utils.FormatUtils;
+import com.chiachen.portfolio.R;
+import com.chiachen.portfolio.utils.tool.TypefaceManager;
 
 
 /**
  * Created by pascal on 2017/10/26.
  */
 
-public class WeightDashboardView extends BasicCustomDrawView {
-    private static final float mBigTextSizeSP = 64f;
-    private static final float mSmallTextSizeSP = 18f;
+public class WeightDashboardView extends View {
     private static final int LINE_COUNT = 100; //总刻度数
-    private int mPercent;
-    private float mStartWeight, mEndWeight, mNowWeight;
-    private String mUnit = "";
+    protected static final int UNDONE_LINE_COLOR = 2660;
+    protected static final int WEIGHT_DONE_LINE_COLOR = 2704;
+
+    protected Paint mPaint;
+    protected int mWidth, mHeight;
 
     public WeightDashboardView(Context context) {
         super(context);
+        initPaint();
     }
 
     public WeightDashboardView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initPaint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        drawText(canvas);
-
-        drawDashboard(canvas, mPercent);
-
-    }
-
-    public void setDashboardData(float nowWeight, float startWeight, float endWeight, String unit, int percent) {
-        mNowWeight = nowWeight;
-        mStartWeight = startWeight;
-        mEndWeight = endWeight;
-        mUnit = unit;
-        mPercent = percent;
-        this.invalidate();
+        drawDashboard(canvas, 30);
     }
 
     private void drawDashboard(Canvas canvas, int percent) {
@@ -56,34 +49,46 @@ public class WeightDashboardView extends BasicCustomDrawView {
         canvas.rotate(45, 0f, 0f);
 
         for (int linePosition = 0; linePosition < LINE_COUNT; linePosition++) {
-
             if (linePosition < percent) {
                 setPaint(WEIGHT_DONE_LINE_COLOR, 0, mHeight / 100f);
             } else {
                 setPaint(UNDONE_LINE_COLOR, 0, mHeight / 100f);
             }
             canvas.drawLine(0f, y, 0f, y + lineSize, mPaint);
-
             canvas.rotate(2.75f, 0f, 0f);
         }
-
-
         canvas.restore();
     }
 
-    private void drawText(Canvas canvas) {
-        canvas.save();
-        //至中
-        canvas.translate(mWidth / 2f, mHeight / 2f);//+ mHeight / 10f
-        //文字
-        setPaint(WEIGHT_SEC_PRIMARY_DARK_COLOR, mSmallTextSizeSP);
-        canvas.drawText(FormatUtils.formatWeight(mStartWeight), -mHeight / 6f, mHeight / 2f - mHeight / 6f, mPaint);
-        canvas.drawText(FormatUtils.formatWeight(mEndWeight), mHeight / 6f, mHeight / 2f - mHeight / 6f, mPaint);
-        setPaint(WEIGHT_PRIMARY_COLOR, mBigTextSizeSP);
-        int textNowHeight = getTextHeight(FormatUtils.formatWeight(mNowWeight), mPaint);
-        canvas.drawText(FormatUtils.formatWeight(mNowWeight), 0f, textNowHeight / 4f + 0f, mPaint);
-        setPaint(WEIGHT_PRIMARY_DARK_COLOR, mSmallTextSizeSP);
-        canvas.drawText(mUnit, 0f, textNowHeight, mPaint);
-        canvas.restore();
+    protected void initPaint() {
+        mPaint = new Paint();
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        this.mWidth = w;
+        this.mHeight = h;
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    protected void setPaint(int color, float textSize, float strokeWidth) {
+        Typeface typeface = TypefaceManager.getTypeface(getContext(), TypefaceManager.GOTHAM_RND_BOLD);
+        switch (color) {
+            case UNDONE_LINE_COLOR: {
+                mPaint.setColor(getResources().getColor(R.color.mainDashboardViewUndoneLine));
+                break;
+            }
+
+            case WEIGHT_DONE_LINE_COLOR: {
+                mPaint.setColor(getResources().getColor(R.color.mainWeightDashboardViewPrimary));
+                break;
+            }
+        }
+
+        mPaint.setStrokeWidth(strokeWidth);
+        mPaint.setTextSize((float) textSize * getResources().getDisplayMetrics().scaledDensity);
+        mPaint.setTypeface(typeface);
     }
 }
