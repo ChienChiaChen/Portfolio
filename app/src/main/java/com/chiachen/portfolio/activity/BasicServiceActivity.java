ced720b21d62f4b751cc9d304a6e103ff9af8c91
JasonChien <s124202468@gmail.com>
2018-07-22 23:56:55 +0800

    1. Add bind and unbind service. 2. Add foreground service.

--- app/src/main/java/com/chiachen/portfolio/activity/BasicServiceActivity.java
+++ app/src/main/java/com/chiachen/portfolio/activity/BasicServiceActivity.java
@@ -1,14 +1,35 @@
 package com.chiachen.portfolio.activity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.chiachen.portfolio.R;
 import com.chiachen.portfolio.service.BasicService;
 
 public class BasicServiceActivity extends AppCompatActivity {
+
+    private BasicService.MyBinder myBinder;
+
+    private ServiceConnection mServiceConnection =new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d("JASON_CHIEN", "\nonServiceConnected");
+            myBinder = (BasicService.MyBinder) service;
+            myBinder.startDownload();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +43,13 @@ public class BasicServiceActivity extends AppCompatActivity {
 
     public void stopService(View view) {
         stopService(new Intent(BasicServiceActivity.this, BasicService.class));
+    }
+
+    public void bindService_(View view) {
+        bindService(new Intent(BasicServiceActivity.this, BasicService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void UnbindService(View view) {
+        unbindService(mServiceConnection);
     }
 }
--- app/src/main/java/com/chiachen/portfolio/service/BasicService.java
+++ app/src/main/java/com/chiachen/portfolio/service/BasicService.java
@@ -1,11 +1,21 @@
 package com.chiachen.portfolio.service;
 
+
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.chiachen.portfolio.R;
+import com.chiachen.portfolio.activity.BasicServiceActivity;
+
 public class BasicService extends Service {
+
+    private MyBinder mBinder = new MyBinder();
 
     public BasicService() {
     }
@@ -18,6 +28,22 @@ public class BasicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Notification.Builder builder = new Notification.Builder(this.getApplicationContext());
+        Intent nfIntent = new Intent(this, BasicServiceActivity.class);
+
+        builder.setContentIntent(PendingIntent.getActivity(this, 0, nfIntent, 0))
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_launcher))
+                .setContentTitle("下拉列表中的Title")// Content title.
+                .setContentText("要顯示的內容") // Content text.
+                .setSmallIcon(R.mipmap.ic_launcher) // Small icon.
+                .setWhen(System.currentTimeMillis()); // Set time.
+
+        Notification notification = builder.build();
+        notification.defaults = Notification.DEFAULT_SOUND;
+
+        startForeground(110, notification);// start foreground service
+
         Log.d("JASON_CHIEN", "\nonStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
@@ -25,12 +51,19 @@ public class BasicService extends Service {
     @Override
     public void onDestroy() {
         Log.d("JASON_CHIEN", "\nonDestroy");
+        stopForeground(true);//Stop foreground service.
         super.onDestroy();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d("JASON_CHIEN", "\nonBind");
+        return mBinder;
+    }
+
+    public class MyBinder extends Binder {
+        public void startDownload() {
+            Log.d("JASON_CHIEN", "\nstartDownload() executed");
+        }
     }
 }
